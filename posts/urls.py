ff264aae53dde6a26ea350e5cca81c526474f2ae
codewizz <c.chaiwat.ch@gmail.com>
2022-04-21 21:23:00 +0000

    update: complete crud functionality

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,10 @@
 # posts/urls.py
 
-from django.urls.import path
+from django.urls import path
 from .views import PostList, PostDetail
 
-
 urlpatterns = [
-    path('<int:pk>', PostDetail.as_view()),
+    path('<int:pk>/', PostDetail.as_view()),
     path('', PostList.as_view()),
 ]
 
